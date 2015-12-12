6c7ad96252b05d78b33944064285d81cf880ada9
mynameis7 <andrew.rather5@gmail.com>
2015-12-12 14:54:47 -0500

    Added bootstrap and navbar

--- app/views.py
+++ app/views.py
@@ -13,10 +13,9 @@ def index():
 
 @app.route('/viewlogs')
 def logsView():
-        logs = app.guild.log
         print "Viewing Logs"
         #logs = logmerger.merge_all_logs(os.getcwd() + '/logs')
         return render_template("logsView.html",
                                title='View Logs',
-                               logs=logs
+                               guild=app.guild
                                )
