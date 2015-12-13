5f98cce74e8d61f18900d8084e576fb5f27d1aab
mynameis7 <andrew.rather5@gmail.com>
2015-12-13 14:41:26 -0500

    Fixed gitignore

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-from app import views
-import LogAnalyzer
-import LogAnalyzer as SK
-from LogAnalyzer import *
-app.guild = SK.Guild()
-app.guild.load_from_file("./League_Of_Gunners.guild")
