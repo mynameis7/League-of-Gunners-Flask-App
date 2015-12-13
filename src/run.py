066493a1871e9996dee55c5d8ff9484f78075821
mynameis7 <andrew.rather5@gmail.com>
2015-12-13 15:31:49 -0500

    Still trying to fix deployment

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from app import app
-from app.LogAnalyzer import *
-app.guild = load_json_guild("./app/static/League_of_Gunners.json-guild")
+#from LogAnalyzer import *
+#app.guild = load_json_guild("./app/static/League_of_Gunners.json-guild")
+app.guild = None
 app.run(debug=True, port=80)
 #app.run(host="0.0.0.0")
