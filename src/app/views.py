0339c843388f4b69ae3b0c4310ccf3249298c50b
mynameis7 <andrew.rather5@gmail.com>
2015-12-14 23:35:50 -0500

    Added an About page

--- src/app/views.py
+++ src/app/views.py
@@ -14,11 +14,11 @@ def get_base_vars():
         return d
 
 def get_members_by_rank():
-        guildmaster = models.Guildmate.query.filter(models.Guildmate.in_guild == True and models.Guildmate.rank_val == 4)
-        officer = models.Guildmate.query.filter(models.Guildmate.in_guild == True and models.Guildmate.rank_val == 3)
-        veteran = models.Guildmate.query.filter(models.Guildmate.in_guild == True and models.Guildmate.rank_val == 2)
-        member = models.Guildmate.query.filter(models.Guildmate.in_guild == True and models.Guildmate.rank_val == 1)
-        recruit = models.Guildmate.query.filter(models.Guildmate.in_guild == True and models.Guildmate.rank_val == 0)
+        guildmaster = models.Guildmate.query.filter(models.Guildmate.rank_val == 4 and models.Guildmate.in_guild == True)
+        officer = models.Guildmate.query.filter(models.Guildmate.rank_val == 3 and models.Guildmate.in_guild == True)
+        veteran = models.Guildmate.query.filter(models.Guildmate.rank_val == 2 and models.Guildmate.in_guild == True)
+        member = models.Guildmate.query.filter(models.Guildmate.rank_val == 1 and models.Guildmate.in_guild == True)
+        recruit = models.Guildmate.query.filter(models.Guildmate.rank_val == 0 and models.Guildmate.in_guild == True)
 
         mems = {"Guild Master": guildmaster,
                 "Officer": officer,
@@ -34,15 +34,14 @@ def get_members_by_rank():
 @app.route('/')
 @app.route('/index')
 def index():
-        base = get_base_vars()
-        mems = get_members_by_rank()
-        
-	return render_template("index.html",
-				title='Home',
+    base = get_base_vars()
+    mems = get_members_by_rank()
+    return render_template("index.html",
+                title='Home',
                                 name=base["name"],
                                 short_name=base["short"],
                                 members=mems
-				)
+                )
 
 @app.route('/viewlogs')
 def logsView():
@@ -57,3 +56,11 @@ def logsView():
                                short_name=base["short"],
                                logs=logs,
                                )
+
+@app.route('/about')
+def about():
+        base = get_base_vars()
+        return render_template("about.html",
+                               title="Charter",
+                               name=base["name"],
+                               short_name=base["short"])
--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@ import sys, os
 path= os.path.abspath(os.path.join("../", "src"))
 if path not in sys.path:
     sys.path.append(path)
-from app import db, models
+from app import db, models, views
 
 mems = db.session.query(models.Guildmate).all()
+mems = views.get_members_by_rank()
