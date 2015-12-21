c4790038349cced227747b025d2bc4c9527d8b52
mynameis7 <andrew.rather5@gmail.com>
2015-12-21 03:28:05 -0500

    Reverted the turbolinks usage.  Fixed a link

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask_turbolinks import turbolinks
+#from flask_turbolinks import turbolinks
 from config import basedir
 
 app = Flask(__name__)
@@ -16,6 +16,6 @@ lm.init_app(app)
 lm.login_view = 'login'
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
-turbolinks(app)
+#turbolinks(app)
 
 from app import views, models
